049ca66b7fa6fa3f0a4cac05b5f62c6bf3f12c54
hungnguyen111 <nguyenhuyhung.ueh@gmail.com>
2020-02-24 10:54:00 +0700

    react app

---
+++ precache-manifest.968deb72be62eebb3b68defe005787a4.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44207fdc939beb62e85379e0af114c2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "015ead9cabce67ad70d4",
+    "url": "/static/css/2.08ec5d0e.chunk.css"
+  },
+  {
+    "revision": "3752990867b21578a2c6",
+    "url": "/static/css/main.ab8ef38b.chunk.css"
+  },
+  {
+    "revision": "015ead9cabce67ad70d4",
+    "url": "/static/js/2.a84f29b4.chunk.js"
+  },
+  {
+    "revision": "639cb9b75c446d796a08fe6b7ddd9583",
+    "url": "/static/js/2.a84f29b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3752990867b21578a2c6",
+    "url": "/static/js/main.a724414d.chunk.js"
+  },
+  {
+    "revision": "b055bc77f6e19ab980bf",
+    "url": "/static/js/runtime-main.fb4fda4a.js"
+  },
+  {
+    "revision": "4640a15caf7a0ff292d45f5c0f125201",
+    "url": "/static/media/09adbe8e1eff4efa227c6e41bfd5b893.4640a15c.jpg"
+  },
+  {
+    "revision": "2b938fd55e90c731e20c7224561b6191",
+    "url": "/static/media/73eb26f02cf1f09fdca7f0ad2ff4d7b6.2b938fd5.jpg"
+  },
+  {
+    "revision": "d713fc3a3e00188abf478fc9ee7a9690",
+    "url": "/static/media/back-news.d713fc3a.png"
+  },
+  {
+    "revision": "5de6f656b882b385e291b50b8ddb7372",
+    "url": "/static/media/backapp.5de6f656.jpg"
+  },
+  {
+    "revision": "67c447ca35df17018b561e97a5717c53",
+    "url": "/static/media/ccee-15768157060937.67c447ca.jpg"
+  },
+  {
+    "revision": "717c0ada3d3d0f046de6576bf279fb9a",
+    "url": "/static/media/cgv1.717c0ada.jpg"
+  },
+  {
+    "revision": "0dc8f4890c6897bf4b741674e2550ba4",
+    "url": "/static/media/cine.0dc8f489.jpg"
+  },
+  {
+    "revision": "5bb41c8ba4abc4a3210d112af781acb6",
+    "url": "/static/media/combo1.5bb41c8b.jpg"
+  },
+  {
+    "revision": "648deac2c569e6f39e70e6403e9c93c6",
+    "url": "/static/media/combo2.648deac2.jpg"
+  },
+  {
+    "revision": "4acc64916d5af37d82d28d1b7ab76226",
+    "url": "/static/media/combo3.4acc6491.jpg"
+  },
+  {
+    "revision": "b2e374134cb3e71d3d178b4f1a3c5666",
+    "url": "/static/media/cum-rap-cgv-chinh-thuc-co-mat-tren-123phim-15761453956040.b2e37413.jpg"
+  },
+  {
+    "revision": "d7c743aee9585a8be2455df5fa8638bb",
+    "url": "/static/media/dcine.d7c743ae.png"
+  },
+  {
+    "revision": "a39ab4de3a8c3fc1aa5631eee65af033",
+    "url": "/static/media/jumanjii-3-15762073170542.a39ab4de.jpg"
+  },
+  {
+    "revision": "16aa16d58eb8a203ba409c6a29a4779d",
+    "url": "/static/media/ke-du-hanh-15761286853539.16aa16d5.jpg"
+  },
+  {
+    "revision": "79cb5c4f3cd3910399daf7af6cfd5e9b",
+    "url": "/static/media/last-christ-15762073893342.79cb5c4f.jpg"
+  },
+  {
+    "revision": "184067dcffebc899e4591756ee08fa58",
+    "url": "/static/media/lotte.184067dc.png"
+  },
+  {
+    "revision": "9ce9c4a07655c09bfad79009ab96b0d5",
+    "url": "/static/media/lotte.9ce9c4a0.jpg"
+  },
+  {
+    "revision": "233320350f4d49daf1a5911722f37e8e",
+    "url": "/static/media/mat-biec-15768091323740.23332035.jpg"
+  },
+  {
+    "revision": "4fd5e51b5efd1e8cce8dce4f5a93a10b",
+    "url": "/static/media/megags.4fd5e51b.jpg"
+  },
+  {
+    "revision": "f26eb7249405edd448dcb0c468bfb993",
+    "url": "/static/media/muon-hinh-van-trang-kieu-doc-than-trong-chong-cu-tinh-moi-15773512253745.f26eb724.jpg"
+  },
+  {
+    "revision": "de64579fae03637b674ab1a98399b849",
+    "url": "/static/media/rambo-hoi-ket-dam-mau-hoi-ket-xung-tam-thuong-hieu-hanh-dong-duoc-yeu-thich-hang-dau-hollywood-15772691049778.de64579f.jpg"
+  },
+  {
+    "revision": "31d8c4486cd3057eac7ba77f820fc8d3",
+    "url": "/static/media/slide0.31d8c448.png"
+  },
+  {
+    "revision": "97d1882c2b8809777fca572c979a49dc",
+    "url": "/static/media/slide1.97d1882c.png"
+  },
+  {
+    "revision": "28b134a5b8ab9dc3c8c5a9e0978be790",
+    "url": "/static/media/slide10.28b134a5.png"
+  },
+  {
+    "revision": "efe3e36fb55ff580b44d001829441306",
+    "url": "/static/media/slide2.efe3e36f.png"
+  },
+  {
+    "revision": "994897fe89bb7fc66bf66800e6fb1b79",
+    "url": "/static/media/slide4.994897fe.png"
+  },
+  {
+    "revision": "6226d72e409aeaa958218986e74ffc42",
+    "url": "/static/media/slide5.6226d72e.png"
+  },
+  {
+    "revision": "65bc3dbc4c158b27e76f906fbaee57b7",
+    "url": "/static/media/slide7.65bc3dbc.png"
+  },
+  {
+    "revision": "88f82185118249552b8a68934df14407",
+    "url": "/static/media/slide8.88f82185.png"
+  },
+  {
+    "revision": "2c0127067cc153e88d817d8e66a90934",
+    "url": "/static/media/slide9.2c012706.png"
+  },
+  {
+    "revision": "2c2e0ae2b9f26d05576425bff02e8435",
+    "url": "/static/media/thong-bao-chao-don-rap-bhd-star-the-garden-ha-noi-tren-tix-15772726310715.2c2e0ae2.jpg"
+  },
+  {
+    "revision": "302b67c578816cd7ed3692c73df3b5a0",
+    "url": "/static/media/thong-bao-doi-ten-thuong-hieu-123phim-15771601850188.302b67c5.jpg"
+  },
+  {
+    "revision": "763342018d52d5bab57b030aeb39b441",
+    "url": "/static/media/tong-hop-tat-ca-nhan-vat-trong-thuong-hieu-dinh-dam-star-wars-p1-15776842659094.76334201.jpg"
+  }
+]);
